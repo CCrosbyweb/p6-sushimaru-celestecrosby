11e487ca96c0fa9854f0725a05a1f5d60e45d4ca
CCrosbyweb <celeste@eagles.ewu.edu>
2014-06-01 20:19:35 -0700

    Version 7

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,20 @@ $(document).foundation();
 
 // On page load, do this
 $(document).ready(function(){
-  
+
+	$('.gallery').magnificPopup({
+	  delegate: 'a', // child items selector, by clicking on it popup will open
+	  type: 'image',
+	  gallery: {enabled:true}
+	  // other options
+	});
+
+
+
+
+
+
+
   // Your Scripts
 
 });
